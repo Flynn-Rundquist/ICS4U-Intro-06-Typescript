a78232406915e3380ce2849b3c4a6e2cfcc1cf58
Flynn Rundquist <flynn.runquist@mths.ca>
2024-02-15 14:54:29 +0000

    Hello, World added

---
+++ index.ts
@@ -0,0 +1,14 @@
+
+/** 
+* World program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* By:     Flynn Rundquist 
+* Version: 1.0
+* Since:   2020-01-01
+*/
+
+console.log("Hello, World!");
+
+console.log("\nDone.");
+
