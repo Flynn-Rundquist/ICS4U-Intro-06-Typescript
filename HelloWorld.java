a5103e2886472684e98708094ff9763aa8f2e228
Flynn-Rundquist <124379094+Flynn-Rundquist@users.noreply.github.com>
2024-02-29 23:00:37 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,13 +7,33 @@
  * @since 2024-02-29
  */
 
+/**
+* This is the standard "Hello, World!" program.
+*/
 final class HelloWorld {
-        private HelloWorld() {
-                throw new IllegalStateException("Cannot be instantiated");
-        }
 
-        public static void main(final String[] args) {
-                System.out.println("Hello, World!");
-                System.out.println("\nDone.");
-        }
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private HelloWorld() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+
+        // print out "Hello, World!"
+        System.out.println("Hello, World!");
+
+        System.out.println("\nDone.");
+    }
 }
